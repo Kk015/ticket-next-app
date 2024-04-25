12b6279fb0fd38069a49da7da53c03347460a091
Kunal Mathur <kunalmathur0803@gmail.com>
2024-04-25 10:13:11 +0530

    inital state

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import TicketPage from "./(components)/TicketCard";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <TicketPage />
+    </div>
+  );
+};
+
+export default Dashboard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,15 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }, colors: {
+        nav: "#18222f",
+        page: "#2b3441",
+        card: "#47566a",
+        "card-hover": "#4f574",
+        "default-text": "#f1f3f5",
+        "blue-accent": "#0084d4",
+        "blue-accent-hover": "#009fff",
+      }
     },
   },
   plugins: [],
